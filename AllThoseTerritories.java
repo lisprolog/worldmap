740afcbb8e15821d93f739740b7283af33d6e695
lisprolog <peter.sattarpoor@gmx.at>
2016-02-17 19:43:10 +0000

    color all patches in territory

--- AllThoseTerritories.java
+++ AllThoseTerritories.java
@@ -10,6 +10,8 @@ import java.awt.geom.GeneralPath;
 import java.util.Arrays;
 
 public class AllThoseTerritories{
+	
+
 
     public static void main(String[] args){
 
@@ -27,6 +29,8 @@ public class AllThoseTerritories{
 }
 
 class PolygonPanel extends JPanel{
+
+    boolean gameOn = true;
 
     HashMap<String, Territory> territoryMap;
     LinkedList<Territory> territories;
@@ -63,11 +67,12 @@ class PolygonPanel extends JPanel{
             initPolygons();
 	}
 
+	String pauseToken = "";
+	
 	path = new GeneralPath(GeneralPath.WIND_NON_ZERO);
 	// create iterator of polygons
 	it = polygons.listIterator(0);
 	while(it.hasNext()){
-//		System.out.println("hasNext()?"+it.hasNext());
 		pa = it.next();
 		Point p1;
 		Point p2;
@@ -79,14 +84,48 @@ class PolygonPanel extends JPanel{
 			path.lineTo(p2.getX(), p2.getY());
 		}
 		path.lineTo(p1.getX(), p1.getY());
-//		g2.setPaint(Color.BLUE);
-//		System.out.println("PaintComp:"+pa.getName() + ": " +pa.getColor());
 		g2.setPaint(pa.getColor());
 		g2.setStroke(new BasicStroke(0.1f));
 		g2.fill(pa);
-//		g2.draw(path);
 	}
-   }  
+
+	while(gameOn){
+		System.out.println("Phase Landerwerb: ");
+//		while(isNotOccupied()){
+			System.out.println("Choose a country!");
+//			pauseToken = sc.next();
+//		}
+		System.out.println("Phase Angreifen und Bewegen: ");
+		System.out.println("Phase Eroberungen: ");
+
+		gameOn = false;		
+	}
+    }
+
+    public boolean isNotOccupied(){
+	it = polygons.listIterator(0);
+	boolean occupied = true;
+	while(it.hasNext()){
+		pa = it.next();
+		if(pa.getColor() != Color.BLACK){
+			occupied = false;
+		}
+	}
+	return occupied;
+    }
+
+    public boolean computerChoose(){
+	it = polygons.listIterator(0);
+	boolean occupied = true;
+	while(it.hasNext()){
+		pa = it.next();
+		if(pa.getColor() == Color.BLACK){
+			occupied = false;
+		}
+	}
+	return occupied;
+    }
+
     private void initPolygons(){
 
 		territories = new LinkedList<Territory>();
@@ -152,14 +191,14 @@ class PolygonPanel extends JPanel{
 //				System.out.println("------->"+name +" "+ t);
 				territoryMap.put(name, t);
 				lastName = name;
-				System.out.println("Patch " + pa.getName()+" created");
+//				System.out.println("Patch " + pa.getName()+" created");
 				token = sc.next();
 
 			}
 
 			if(token.equals("capital-of")){
 
-				System.out.println("startNewCapital");
+//				System.out.println("startNewCapital");
 
 				name = sc.next();
 				while(!sc.hasNextInt()){
@@ -169,28 +208,28 @@ class PolygonPanel extends JPanel{
 				Capital c = new Capital(sc.nextInt(),sc.nextInt());
 				System.out.println("capital-of:" + name);
 				t = territoryMap.get(name);
-				System.out.println("Map:getTerritory: "+t.getName());
+//				System.out.println("Map:getTerritory: "+t.getName());
 				t.setCapital(c);
 				territoryMap.put(name, t);
-				System.out.println("setCapitalMaptest: "+territoryMap.get(name).getCapital());
+//				System.out.println("setCapitalMaptest: "+territoryMap.get(name).getCapital());
 				token = sc.next();
 			}
 
 			if(token.equals("neighbors-of")){
-				System.out.println("scNext():" + token);
+//				System.out.println("scNext():" + token);
 				patchName = getTerritoryName();
-				System.out.println("NEIGHBORSOF:Territory:"+ patchName);
-				System.out.println("#"+patchName+"#");
-				System.out.println("contains: "+territoryMap.containsKey(patchName));
+//				System.out.println("NEIGHBORSOF:Territory:"+ patchName);
+//				System.out.println("#"+patchName+"#");
+//				System.out.println("contains: "+territoryMap.containsKey(patchName));
 				t = territoryMap.get(patchName);
-				System.out.println(territoryMap.get(patchName));
-				System.out.println("t:" + t);
+//				System.out.println(territoryMap.get(patchName));
+//				System.out.println("t:" + t);
 				// get name of neighbors
 				String[] neighborNamesArray;
 				neighborNamesArray = getNeighborName();
-				System.out.println("ArraySize: "+neighborNamesArray.length);
+//				System.out.println("ArraySize: "+neighborNamesArray.length);
 				t.addNeighbors(neighborNamesArray);
-				System.out.println("Neighbors added!");				
+//				System.out.println("Neighbors added!");				
 			}// end neighbors-of
 
 			if(token.equals("continent")){
@@ -213,13 +252,12 @@ class PolygonPanel extends JPanel{
 			}// end continent
 		}while(sc.hasNext());// end while(sc.hasNext())
 //		FILE INPUT END		
-//////////////////////////////////////////////////////////////////
 
 //		fill the map the first time
 		path = new GeneralPath(GeneralPath.WIND_NON_ZERO);
 		// create iterator of polygons
 		it = polygons.listIterator(0);
-		System.out.println("start drawing");
+//		System.out.println("start drawing");
 		while(it.hasNext()){
 			pa = it.next();
 			Point p1;
@@ -236,9 +274,6 @@ class PolygonPanel extends JPanel{
 			g2.setStroke(new BasicStroke(0.1f));
 			g2.fill(pa);
 		}
-
-//////////////////////////////////////////////////////////////////////////
-
  	}// end initPolygon()
   
 /**	the Territory names can have 1(ex "Japan") 2 (ex: "Central America") or 3 tokens(ex: "Western United States")
@@ -267,23 +302,23 @@ class PolygonPanel extends JPanel{
 	this function returns a String Array containing all neighbors of one Territory
 */
 	private String[] getNeighborName(){
-		System.out.println("startNeighborNameFunktion!");
+//		System.out.println("startNeighborNameFunktion!");
 		LinkedList<String> list = new LinkedList<String>();
 		String[] s;
 		token = "";// = sc.next();
 		String nName = "";// = ""; //nToken; 
 		while(!token.equals("neighbors-of")&& !token.equals("continent")&& sc.hasNext()){
 			do{
-			System.out.println("test!" + nName);
+//			System.out.println("test!" + nName);
 				token = sc.next();
-				System.out.println(token);
+//				System.out.println(token);
 				if(!token.equals("-") && !token.equals("neighbors-of")&& !token.equals("continent") && !token.equals("")){
 					nName += token + " ";
-					System.out.println("Name: "+nName);
-					System.out.println("test2");
+//					System.out.println("Name: "+nName);
+//					System.out.println("test2");
 				}
 			}while(!token.equals("-") && !token.equals("neighbors-of")&& !token.equals("continent") && !token.equals("continent") && sc.hasNext());
-			System.out.println("NeighborName = " + nName);
+//			System.out.println("NeighborName = " + nName);
 			list.add(nName);
 			nName = "";
 		}
@@ -319,6 +354,7 @@ class PolygonPanel extends JPanel{
 /**
   *	
   */
+
 class PolygonSelector extends MouseAdapter{
 
     PolygonPanel polygonPanel;
@@ -339,13 +375,54 @@ class PolygonSelector extends MouseAdapter{
 		if(patch.contains(p)){					// if patch contains Point
 			polyName = patch.name;
 			i = polygonPanel.polygons.indexOf(patch);	// get the index of patch in list of polygons
-			System.out.println(i+":click:"+patch.name+ " Farbe:"+ patch.getColor());
 			patch = polygonPanel.polygons.get(i);		// get the patch
-			patch.setColor(Color.BLUE);			// set the color
-			polygonPanel.polygons.set(i, patch);		// set the patch back into the list
-//			patch = polygonPanel.polygons.get(i);		
+			if(isPatchFree(Color.BLUE, patch)){
+				patch.setColor(Color.BLUE);			// set the color
+				polygonPanel.polygons.set(i, patch);		// set the patch back into the list
+			}
 			polygonPanel.repaint();				// repaint the picture/panel
 		}
 	}
+	System.out.println(polyName);
+	it = polygonPanel.polygons.listIterator(0);
+	while(it.hasNext()){
+		patch = it.next();
+		if(patch.getName().equals(polyName)){	
+			i = polygonPanel.polygons.indexOf(patch);
+			patch = polygonPanel.polygons.get(i);
+			patch.setColor(Color.BLUE);
+			polygonPanel.polygons.set(i, patch);
+		}
+		polygonPanel.repaint();				
+	}
+    }
+
+    public boolean isPatchFree(Color c, Patch pa){
+	boolean free = false;
+	Patch patch = pa;;
+	if(patch.getColor() == Color.BLACK /*|| patch.getColor() == c*/){
+		System.out.println("FreePatch");
+		free = true;
+	}else{
+		System.out.println("Patch not free");
+	}
+	return free;
+    }
+
+    public boolean occupationPhase(){
+
+	Patch patch;
+	String polyName = "";
+	ListIterator<Patch> it = polygonPanel.polygons.listIterator(0);
+	boolean occupied = true;
+	while(it.hasNext()){
+		patch = it.next();
+		if(patch.getColor() == Color.BLACK){					// if patch contains Point
+			occupied = true;
+		}
+	}
+	return occupied;
     }	
+
+
 }
